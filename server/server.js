bb43781303092e609d346b9c0bdbc882de62c5e5
iamniteeshmishra@gmail.com <nitish219@gmail.com>
2019-02-10 20:53:12 +0530

    Final Commit

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
 
 const { mongoose, ObjectID } = require('../db/mongoose');
 const { todo } = require('../models/todos');
@@ -99,6 +100,87 @@ app.get('/todos/:id', (req, res) => {
       res.status(404).send(err.message);
     });
 })
+
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send('Id is not Valid. Please Enter a Valid ID');
+  }
+  todo.findByIdAndDelete(id).then((docs) => {
+    if (docs) {
+      res.status(200).send({ docs });
+    }
+    else {
+      res.status(404).send('No Matching Record Found');
+    }
+  }, (err) => {
+    res.status(404).send(err.message);
+  });
+});
+
+app.delete('/users/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send('Id is not Valid. Please Enter a Valid ID');
+  }
+  user.findByIdAndDelete(id).then((docs) => {
+    if (docs) {
+      res.status(200).send({ docs });
+    }
+    else {
+      res.status(404).send('No Matching Record Found');
+    }
+  }, (err) => {
+    res.status(404).send(err.message);
+  });
+});
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send('Id is not Valid. Please Enter a Valid ID');
+  }
+
+  if (_.isBoolean(body.completed && body.completed == true)) {
+    body.completedAt = new Date().toString();
+  }
+  else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((docs) => {
+    if (docs) {
+      res.status(200).send({ docs });
+    }
+    else {
+      res.status(404).send('No Matching Record Found');
+    }
+  }, (err) => {
+    res.status(404).send(err.message);
+  });
+});
+
+
+app.patch('/users/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['name', 'email']);
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send('Id is not Valid. Please Enter a Valid ID');
+  }
+
+  user.findByIdAndUpdate(id, { $set: body }, { new: true }).then((docs) => {
+    if (docs) {
+      res.status(200).send({ docs });
+    }
+    else {
+      res.status(404).send('No Matching Record Found');
+    }
+  }, (err) => {
+    res.status(404).send(err.message);
+  });
+});
 
 app.listen(port, () => {
   console.log('Listning to port ' + port)
